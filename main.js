ee6a6b4d42e151d2fc5befdeddd454f44b9770f8
johnmartinezc <john.martinez@codeimmersives.com>
2022-10-12 15:56:00 -0400

    -a

--- main.js
+++ main.js
@@ -1,6 +1,87 @@
 // Your code here.
 
+let person = {
+  firstName: 'john',
+  lastName: 'Martinez',
+  age: 28,
+  married: "yes",
+  spouseName: "jas alverez"
+  
+  
+}
 
+let person2 = {
+  firstName: 'jas',
+  lastName: 'alverez',
+  married: "yes",
+  spouseName: "john Martinez"
+}
+
+function getFirstName(person){
+return person.firstName
+}
+// console.log(getFirstName({firstName: 'fisher'}))
+
+function getLastName(person){
+  return person.lastName
+}
+// console.log(getFirstName({lastName: 'fisher'}))
+
+function getFullName(person){
+  //  let person = firstName + lastName
+
+  return person.firstName + " "+ person.lastName
+}
+// console.log(getFullName({firstName: 'john', lastName: 'Martinez'}))
+
+
+function setFirstName(person, newFirstName){
+  person.firstName = newFirstName
+
+}
+setFirstName(person, 'zuko')
+// console.log(person)
+
+
+function setAge(person, newAge){
+person.age = newAge
+}
+setAge(person, 30)
+// console.log(person)
+
+
+function giveBirthday(person){
+  if(person.age === undefined){
+    person.age = 1
+  }else if(person.age !== undefined){
+    person.age += 1  }
+}
+giveBirthday(person)
+giveBirthday(person2)
+// console.log(person)
+// console.log(person2)
+
+function marry(person, person2){
+    if(person.married === false && person2.married === false)
+    person.married = true
+    person2.married = true
+    person.spouseName = getFullName(person2)
+    person2.spouseName = getFullName(person)
+
+
+}
+marry(person, person2)
+console.log(person,person2)
+
+
+function divorce(person, person2){
+  person.married = false
+  person2.married = false
+  delete person.spouseName
+  delete person2.spouseName
+}
+divorce(person,person2)
+console.log(person, person2)
 
 
 
